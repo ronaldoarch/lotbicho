00e5394cd19c321f8973b2a864a117dca332efe4
Ronaldo dias de sousa <ronaldohunter54@gmail.com>
2026-01-14 16:10:03 -0300

    fix: remover div extra no HeroBanner

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -219,7 +219,6 @@ export default function HeroBanner() {
                   </div>
                 </>
               )}
-              </div>
             </div>
             </div>
           </SwiperSlide>
